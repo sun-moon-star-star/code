058fc76d0815eb1450bc4f60c09b12a399ee5f4a
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-04-06 18:21:27 +0800

    第 45 届国际大学生程序设计竞赛（ICPC）亚洲区域赛（济南）: StoneGame.cc

---
+++ icpc/第 45 届国际大学生程序设计竞赛（ICPC）亚洲区域赛（济南）/StoneGame.cc
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <algorithm>
+
+int main()
+{
+    long long a1, a2, a3, ans;
+    while (~scanf("%lld%lld%lld", &a1, &a2, &a3))
+    {
+        ans = 0;
+        if (a1 && a2)
+        {
+            if (a1 > a2)
+            {
+                ans += a2 * 2;
+                a1 -= a2;
+                a2 = 0;
+            }
+            else
+            {
+                ans += a1 * 2;
+                a2 -= a1;
+                a1 = 0;
+            }
+        }
+        if (a1 > 1)
+        {
+            ans += a1 / 3 * 3;
+            if (a1 % 3 == 2)
+            {
+                ans += 1
+            }
+        }
+        if (a2 > 1)
+        {
+            ans += a2 / 3 * 6;
+            if (a2 % 3 == 2)
+            {
+                ans += 4;
+            }
+        }
+
+        printf("%lld\n", ans);
+    }
+    return 0;
+}
